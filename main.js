455c715188f44c5a01e510f36458d16a24cf5643
Matt Ginter <mattginter@gmail.com>
2021-12-10 16:36:05 +0100

    Extra 1-4

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// *********** DECLARATION OF VARIABLES ***********
+
 const x = "John"
 const y = "Doe"
 const testObject = {name: "Mary", surname: "Lamb", email: "maryhad@little.lamb"}
@@ -6,18 +8,13 @@ let randomArr = []
 let arrEx28 = []
 const container = document.getElementById("container")
 const allTDs = document.getElementsByTagName("td")
+const everyLink = document.getElementsByClassName("all-links")
 
-console.log("Page loaded")
+// *********** EVENT LISTENERS ***********
 
-console.log(x + " <> " + y)
 
-console.log(testObject)
 
-delete testObject.email
-
-console.log(testObject)
-
-console.log(stringArr)
+// *********** DECLARATION OF FUNCTIONS ***********
 
 for (let i = 0; i < stringArr.length; i++) console.log(stringArr[i])
 
@@ -30,16 +27,13 @@ const fillArr = (range) =>{
 }
 
 const arrEx26 = fillArr(100)
-console.log(arrEx26)
 
-const findMax = (arr) => {
+const findMinMax = (arr) => {
     const min = Math.min(...arr)
     const max = Math.max(...arr)
     console.log("Min is: " + min)
     console.log("Max is: " + max)
 }
-
-findMax(randomArr)
 
 const arrayOfArr = (range) =>{
     for (let i = 0; i < range; i++){
@@ -48,70 +42,49 @@ const arrayOfArr = (range) =>{
     return arrEx28
 }
 
-arrayOfArr(3)
-
 const longestArr = (arr1, arr2) => arr1.length > arr2.length ? arr1 : arr2
 
-console.log(longestArr(arrEx26, arrEx28))
-
 const sumArr = (arr) =>{
-    console.log(arr)
     let length = arr.length
     let result = 0
     for (let i = 0; i < length; i++){
         const lastElement = arr.pop()
         result += lastElement}
     console.log(result)
-    return result}
+    return result
+}
 
 const biggerSum = (arr1, arr2) => arr1 > arr2 ? "arrayOne" : "arrayTwo" // how to do this since the original array has been mutated... grrr
 
-console.log("EX30")
-const arrayOne = fillArr(2)
-const arrayTwo = fillArr(2)
-
-console.log(biggerSum(sumArr(arrayOne), sumArr(arrayTwo)))
-
-console.log(container)
-
-console.log(allTDs)
-console.log(allTDs.length)
-
-for (let i = 0; i < allTDs.length; i++){
-    allTDs[i].innerHTML = "text"
-}
+// for (let i = 0; i < allTDs.length; i++){
+//     allTDs[i].innerHTML = "text"
+// }
 
 const changeHeading = (newHeading) => {
     let heading = document.querySelector(".heading")
-    heading.innerText = newHeading}
-
-changeHeading("JS Inserted Heading")
+    heading.innerText = newHeading
+}
 
 const insertRow = () => {
     const newRow = document.createElement("tr")
     console.log(newRow)
     newRow.innerText = "JS"
     console.log(newRow)
-    document.getElementById("main-table").appendChild(newRow)}
-
-insertRow()
+    document.getElementById("main-table").appendChild(newRow)
+}
 
 const addClassToRow = (className) =>{
     const rowArray = document.getElementsByTagName("tr")
     console.log(rowArray)
     for (let i = 0; i < rowArray.length; i++){
-        rowArray[i].classList.add(className)}
-}
-
-addClassToRow("red")
+        rowArray[i].classList.add(className)
+}}
 
 const addRedBackground = () => {
     const allLinks = document.getElementsByTagName("a")
     for (let i = 0; i < allLinks.length; i++){
         allLinks[i].style.background = "red"
 }}
-
-addRedBackground()
 
 const addItemsToUl = (itemText) => {
     const newItem = document.createElement("li")
@@ -122,3 +95,68 @@ const addItemsToUl = (itemText) => {
 const emptyList = () => {
     document.getElementById("ulist").innerHTML = ""
 }
+
+// Extra 1
+const selectLinks = () =>{
+    for (let i = 0; i < everyLink.length; i++){
+    const element = everyLink[i]
+    element.addEventListener("mouseover", () => {alert(element)} )
+}}
+
+// Extra 2
+const hideImages = () =>{
+    const images = document.getElementsByTagName("img")
+    for (let i = 0; i < images.length; i++){
+        images[i].classList.add("hide")
+}}
+
+// Extra 3 //easy because there is only one element
+const toggleTable = () => {
+    const tableElement = document.getElementById("main-table")
+    tableElement.classList.toggle("hide")
+}
+
+const getArrayOfTableNumbers = () => {
+    let sum = 0
+    let result = []
+    for (let i = 0; i < allTDs.length; i++){
+        const insideTD = allTDs[i].innerText
+        const parsed = parseInt(insideTD)
+        if (typeof parsed === "number"){
+            result.push(insideTD)
+            result = result.filter(element => element !== "")}}
+    result = result.filter(element => element !== "")
+    let numberString = result.map((i) => Number(i))
+    sum = sumArr(numberString)
+    return sum
+}
+
+// const arrStr = ["1", "3", "5", "9"];
+// const nuevo = arrStr.map((i) => Number(i));
+// console.log(nuevo);
+
+// *********** INITIALIZATION ***********
+
+console.log(x + " <> " + y)
+console.log(testObject)
+delete testObject.email
+console.log(testObject)
+console.log(stringArr)
+console.log(arrEx26)
+findMinMax(randomArr)
+arrayOfArr(3)
+console.log(longestArr(arrEx26, arrEx28))
+console.log("EX30")
+const arrayOne = fillArr(2)
+const arrayTwo = fillArr(2)
+console.log(biggerSum(sumArr(arrayOne), sumArr(arrayTwo)))
+console.log(container)
+console.log(allTDs)
+console.log(allTDs.length)
+changeHeading("JS Inserted Heading")
+insertRow()
+addClassToRow("red")
+addRedBackground()
+selectLinks()
+
+console.log("Page loaded")
