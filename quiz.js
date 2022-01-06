ad8ff11beb1ea3ca7681a514bea272d9a03a5971
Matt Ginter <mattginter@gmail.com>
2022-01-06 01:30:32 +0100

    validate form

--- quiz.js
+++ quiz.js
@@ -98,41 +98,72 @@ const questions = [
         },
 ]
 const displayContainer = document.getElementById("question-div")
+const mainBtn = document.getElementById('main-btn')
+const correctCount = 0
 
 
 const renderQandA = function () {
-    const answerWrapper = document.createElement("div")
-    const index = Math.floor(Math.random() * questions.length)
-    const question = questions.splice(index, 1)
-    const questionWrapper = document.getElementById("question")
-    questionWrapper.innerText = question[0].question
-    answerWrapper.classList.add("answers")
-    const answers = question[0].incorrect_answers
-    console.log(answers)
-    answers.push(question[0].correct_answer)
-    console.log(answers)
-    answers.sort(() => Math.random() - 0.5);
-    for (let i = 0; i < answers.length; i++){
-        const option = document.createElement('div')
-        const input = document.createElement('input')
-        input.setAttribute("type", "radio")
-        input.setAttribute("name", "answer")
-        // console.log(input)
-        option.appendChild(input)
-        const optionItem = document.createElement('div')
-        optionItem.innerText = answers[i]
-        option.appendChild(optionItem)
-        option.classList.add("option")
-        answerWrapper.appendChild(option)
+    if (questions.length !== 0) {
+        const answerWrapper = document.createElement("div")
+        const index = Math.floor(Math.random() * questions.length)
+        const question = questions.splice(index, 1)
+        const questionWrapper = document.getElementById("question")
+        questionWrapper.innerText = question[0].question
+        answerWrapper.classList.add("answers")
+        const answers = question[0].incorrect_answers
+        answers.push(question[0].correct_answer)
+        answers.sort(() => Math.random() - 0.5);
+        for (let i = 0; i < answers.length; i++){
+            const option = document.createElement('div')
+            const input = document.createElement('input')
+            input.setAttribute("type", "radio")
+            input.setAttribute("name", "answer")
+            const optionId = `answer${i}`
+            input.setAttribute("id", optionId)
+            option.appendChild(input)
+            const optionItem = document.createElement('label')
+            optionItem.setAttribute("for", optionId)
+            optionItem.innerText = answers[i]
+            option.appendChild(optionItem)
+            option.classList.add("option")
+            answerWrapper.appendChild(option)
+        }
+        displayContainer.insertBefore(answerWrapper, null)
+        mainBtn.innerText = "Next Question"
+        mainBtn.setAttribute("onclick", "nextQuestion()")
+        return question[0].correct_answer
+        
+    } else {
+        displayResult()
     }
-    displayContainer.insertBefore(answerWrapper, null)
-    const mainBtn = document.getElementById('main-btn')
-    mainBtn.innerText = "Next Question"
-    mainBtn.setAttribute("onclick", "nextQuestion()")
-    console.log(questions.length)
+}
+
+const validateAnswer = () => {
+    const answer = document.querySelector('input[name=answer]:checked')
+    // if(answer.nextElementSibling.innerHTML === )
+    // console.log(answer)
+    const inputs = document.getElementsByName("answer")
+    let formValid = false
+
+    let i = 0
+    while (!formValid && i < inputs.length) {
+        if (inputs[i].checked) formValid = true
+        i++
+    }
+
+    if (!formValid) alert("Please select an answer!")
+    return formValid
 }
 
 const nextQuestion = function () {
+    validateAnswer()
     displayContainer.removeChild(displayContainer.lastChild)
     renderQandA()
+}
+
+const displayResult = function () {
+    mainBtn.remove()
+    displayContainer.innerHTML = ""
+    const resultWrapper = document.createElement('div')
+    // resultWrapper.innerHTML = `You scored ${} out of `
 }
